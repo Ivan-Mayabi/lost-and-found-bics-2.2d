88a9b2e77bb93c1a5dbea721d993736e4165e1ed
syd-xo <sydneyaisha4@gmail.com>
2025-11-21 09:52:34 +0300

    fix: add missing LostAndFoundManagerController import in routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
-
+use App\Http\Controllers\LostAndFoundManagerController;
 use App\Http\Controllers\RoleController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
